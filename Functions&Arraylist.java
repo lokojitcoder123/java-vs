6efba445b90e52af6d0427d2cfb84e91bbe56331
Lokojit Biswas <lokojitb@gmail.com>
2026-02-03 23:30:50 +0530

    Update and rename Functions.java to Functions&Arraylist.java

---
+++ Functions&Arraylist.java
@@ -0,0 +1,137 @@
+
+/*import java.util.*;
+public class Main {
+   public static void main(String[] args){
+     ArrayList<Integer> arr =new ArrayList<>(6);
+     arr.add(0,10);
+     arr.add(1,20);
+     arr.add(2,30);
+     arr.add(3,40);
+     arr.add(4,50);
+     System.out.println(arr);
+     //for(int i=0;i<=5;i++){
+     // System.out.println(arr.get(i)+" ");
+     //}
+
+     System.out.println();
+     arr.set(2,300);
+    // for(int i=0;i<=5;i++){
+     // System.out.println(arr.get(i)+" ");  
+   }
+}*/
+/*import java.util.*;
+public class Main {
+   public static void main(String[] args){
+     ArrayList<Integer> list =new ArrayList<>();
+     System.out.println(list+" "+list.size());
+     list.add(60);
+     System.out.println(list+" "+list.size());
+     list.add(20);
+     System.out.println(list+" "+list.size());
+
+     list.remove(1);
+     System.out.println(list+" "+list.size());
+   }
+}*/
+/*public class Main {
+   public static void main(String[] args){
+   int[] arr ={10,20,30,40,50,60,70,80};
+   int n =arr.length;
+   for(int ele:arr){
+      System.out.print(ele+" ");
+   }
+   System.out.println();
+
+   //reverse: 
+   int i=1 , j=5;
+   while(i<=j){
+      int temp = arr[i];
+      arr[i]=arr[j];
+      arr[j]=temp;
+      i++;
+      j--;
+
+   }for(int ele:arr){
+      System.out.print(ele+" ");
+   }
+   System.out.println();
+   } 
+   //public static void swap(int[] arr, int i ,int j) {
+   //   int temp =arr[i];
+   //   arr[i]=arr[j];
+   //   arr[j]=temp;
+   //}   
+}*/
+
+
+// FUNCTION AND METHODS  
+
+/*import java.util.*;
+
+public class Functions {
+   public static  void printMyName(String name){
+          System.out.println(name);
+          return;
+  }
+
+  public static void main(String args[]){
+     Scanner sc =new Scanner(System.in);
+     String name =sc.next();
+
+     printMyName(name);
+  }
+}*/
+
+
+/*import java.util.*;
+
+public class Functions {
+   public static  int calculateSum(int a ,int b){
+          int sum =a+b;
+          return sum;
+  }
+
+  public static void main(String args[]){
+     Scanner sc =new Scanner(System.in);
+     int a =sc.nextInt();
+     int b =sc.nextInt();
+     
+     int sum =calculateSum(a, b);
+     System.out.println(sum);
+  }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
